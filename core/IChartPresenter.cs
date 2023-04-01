2d4622fe727557d74862899f8cc2962bd58577ce
vladcto <razrab.ytka@gmail.com>
2023-04-01 22:08:51 +0300

    Initial commit. Создал абстрактный класс для команды. Начал писать presenter`а и интерфейс.

---
+++ core/IChartPresenter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Lalalend_3.core
+{
+    public interface IChartPresenter
+    {
+        void ShowChart(Series series);
+        void ShowAdditionalInfo(string info);
+        void ShowGrid(List<string> columnsName, List<List<string>> rows);
+    }
+}
---
+++ core/commands/AbstractChartCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Lalalend_3.core.commands
+{
+    internal abstract class AbstractChartCommand
+    {
+        readonly string filePath;
+
+        public AbstractChartCommand(String path)
+        {
+            filePath = path;
+        }
+
+        public abstract void Run(IChartPresenter presenter);
+    }
+}

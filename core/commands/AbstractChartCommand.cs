7e63143d61d7215582a7c8fe0904ce7b018b9db1
vladcto <razrab.ytka@gmail.com>
2023-04-01 23:05:19 +0300

    Заменил абстрактный класс команды на интерфейс.

--- core/commands/AbstractChartCommand.cs
+++ core/commands/AbstractChartCommand.cs
@@ -5,23 +5,15 @@ namespace Lalalend_3.core.commands
     /// <summary>
     /// Команда отображения статистики.
     /// </summary>
-    internal abstract class AbstractChartCommand
+    public interface IChartCommand
     {
-        /// <summary>
-        /// Текущие данные статистики.
-        /// </summary>
-        readonly string data;
-
         /// <param name="data">Текст статистики.</param>
-        public AbstractChartCommand(String data)
-        {
-            this.data = data;
-        }
+        IChartCommand FromCSV(String data);
 
         /// <summary>
         /// Выполнить команду.
         /// </summary>
         /// <param name="presenter">Презентер, отвечающий за отображение на экране.</param>
-        public abstract void Run(IChartPresenter presenter);
+        void Run(IChartPresenter presenter);
     }
 }
---
+++ core/presenter/ChartPresenter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Lalalend_3.core
+{
+    internal class ChartPresenter : IChartPresenter
+    {
+        public void ShowAdditionalInfo(string info)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowChart(Series series)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowGrid(List<string> columnsName, List<List<string>> rows)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

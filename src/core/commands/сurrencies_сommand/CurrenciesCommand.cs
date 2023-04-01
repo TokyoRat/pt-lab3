ee414afc67ab8c1af7fb44fe0f7579d175452500
TokyoRat <129619607+TokyoRat@users.noreply.github.com>
2023-04-02 00:04:44 +0300

    Добавила CurrenciesCommand и фабрику

---
+++ src/core/commands/сurrencies_сommand/CurrenciesCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lalalend_3.core.commands
+{
+    internal class CurrenciesCommand : IChartCommand
+    {
+        public List<List<string>> data;
+        public CurrenciesCommand(List<List<string>> data)
+        {
+            this.data = data;
+        }
+
+        public void Run(IChartPresenter presenter)
+        {
+            presenter.ShowGrid(new List<string>() { "Дата", "Курс к доллару", "Курс к евро" }, data);
+        }
+    }
+}
---
+++ src/core/commands/сurrencies_сommand/CurrenciesCommandFactory.cs
@@ -0,0 +1,19 @@
+﻿using Lalalend_3.core.commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lalalend_3.src.core.commands.сurrencies_сommand
+{
+    internal class CurrenciesCommandFactory : AbstractCommandFactory
+    {
+        public override IChartCommand CreateFromCSV(string csv)
+        {
+            List<List<string>> list = (List<List<string>>)csv.Split('\n').Select((e) => e.Split(';'));
+
+            return new CurrenciesCommand(list);
+        }
+    }
+}
